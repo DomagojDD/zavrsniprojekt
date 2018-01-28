890829c4ae4531d113a0cd95e4ee367d4d21ca1e
DomagojDD <35570281+DomagojDD@users.noreply.github.com>
2018-01-28 17:21:33 +0100

    Add files via upload

---
+++ js/open_photo.js
@@ -0,0 +1,72 @@
+function myFunction1() {
+    window.open("img/works/1_1.jpg");
+}
+
+function myFunction2() {
+    window.open("img/works/2_1.jpg");
+}
+
+function myFunction3() {
+    window.open("img/works/3_1.jpg");
+}
+
+function myFunction4() {
+    window.open("img/works/4_1.jpg");
+}
+
+function myFunction5() {
+    window.open("img/works/5_1.jpg");
+}
+
+function myFunction6() {
+    window.open("img/works/6_1.jpg");
+}
+
+function myFunction7() {
+    window.open("img/works/7_1.jpg");
+}
+
+function myFunction8() {
+    window.open("img/works/8_1.jpg");
+}
+
+function myFunction9() {
+    window.open("img/works/9_1.jpg");
+}
+
+function myFunction10() {
+    window.open("img/works/10_1.jpg");
+}
+
+function myFunction11() {
+    window.open("img/works/11_1.jpg");
+}
+
+function myFunction12() {
+    window.open("img/works/12_1.jpg");
+}
+
+function myFunction13() {
+    window.open("img/works/13_1.jpg");
+}
+
+function myFunction14() {
+    window.open("img/works/14_1.jpg");
+}
+
+function myFunction15() {
+    window.open("img/works/15_1.jpg");
+}
+
+function myFunction16() {
+    window.open("img/works/16_1.jpg");
+}
+
+function myFunction17() {
+    window.open("img/works/17_1.jpg");
+}
+
+function myFunction18() {
+    window.open("img/works/18_1.jpg");
+}
+
